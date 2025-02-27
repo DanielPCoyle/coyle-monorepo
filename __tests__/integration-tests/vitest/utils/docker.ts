cb4624c534025537de38e54af0ac0b2b2bd681c2
DanielCoyle <dancoyle89@gmail.com>
2025-02-26 21:36:28 -0500

    __test__

--- __tests__/integration-tests/vitest/utils/docker.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-
-import type { StartedPostgreSqlContainer } from '@testcontainers/postgresql';
-import { PostgreSqlContainer } from '@testcontainers/postgresql';
-
-export async function startPostgresContainer(): Promise<StartedPostgreSqlContainer> {
-  return new PostgreSqlContainer().withReuse().start();
-}
