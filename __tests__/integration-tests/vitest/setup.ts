11d982103fa6f5a28240e32b1e4e1df1fe3d6277
DanielCoyle <dancoyle89@gmail.com>
2025-02-26 21:35:53 -0500

    removed setup

--- __tests__/integration-tests/vitest/setup.ts
+++ __tests__/integration-tests/vitest/setup.ts
@@ -8,31 +8,7 @@ import { getPool } from '@backend-api/admin-db/db';
 const logger = debug('setup');
 
 export async function setup() {
-  // logger('Starting PostgreSQL server...');
-  // const container = await startPostgresContainer();
-  // const connection = container.getConnectionUri();
-  // logger(`Started PostgreSQL server at "${connection}"!`);
 
-  // const [adminDb] = await createDatabases(connection);
-
-  // process.env.ADMIN_DB_HOST = container.getHost();
-  // process.env.ADMIN_DB_PORT = container.getPort().toString();
-  // process.env.ADMIN_DB_USERNAME = container.getUsername();
-  // process.env.ADMIN_DB_PASSWORD = container.getPassword();
-  // process.env.ADMIN_DB_NAME = adminDb;
-
-  // process.env.DISABLE_CACHE = 'true';
-  // process.env.LOG_LEVEL = 'error';
-
-  // // process.env.FASTIFY_SESSION_SECRET = 'RANDOM_STRING_WITH_AT_LEAST_32_CHARS';
-
-  // process.env.IMAGE_BUCKET_NAME = 'hashdash-tests';
-
-  // process.env.RECOMMENDATIONS_API_BASE_URL = 'http://recommendations-api/';
-  // process.env.RECOMMENDATIONS_API_AUTH = 'recommendations-api-auth';
-
-  // logger('Migrating database...');
-  // logger('Migrated database', await runMigrations());
 }
 
 export async function teardown() {
