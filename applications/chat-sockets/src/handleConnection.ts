788c5d9af9a847c943dd14cf14578bb1eefb5d52
DanielCoyle <dancoyle89@gmail.com>
2025-02-28 14:13:05 -0500

    import organized

--- applications/chat-sockets/src/handleConnection.ts
+++ applications/chat-sockets/src/handleConnection.ts
@@ -1,15 +1,15 @@
 import { getDB, schema } from "@coyle/database";
 import { Server, Socket } from "socket.io";
-import { addReaction } from "./addReaction.js";
-import { chatMessage } from "./chatMessage.js";
-import { disconnect } from "./disconnect.js";
-import { join } from "./join.js";
-import { leave } from "./leave.js";
-import { login } from "./login.js";
-import { mouseMoveUpdatePeopleOnSite } from "./mouseMoveUpdatePeopleOnSite.js";
-import { seen } from "./seen.js";
-import { updateMessageAction } from "./updateMessageAction.js";
-import { userTyping } from "./userTyping.js";
+import { addReaction } from "./socketHandlers/addReaction.js";
+import { chatMessage } from "./socketHandlers/chatMessage.js";
+import { disconnect } from "./socketHandlers/disconnect.js";
+import { join } from "./socketHandlers/join.js";
+import { leave } from "./socketHandlers/leave.js";
+import { login } from "./socketHandlers/login.js";
+import { mouseMoveUpdatePeopleOnSite } from "./socketHandlers/mouseMoveUpdatePeopleOnSite.js";
+import { seen } from "./socketHandlers/seen.js";
+import { updateMessageAction } from "./socketHandlers/updateMessageAction.js";
+import { userTyping } from "./socketHandlers/userTyping.js";
 
 const {conversations:convos, messages} = schema;
 
