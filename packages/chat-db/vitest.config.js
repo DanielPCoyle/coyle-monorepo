c7e78a277bedcb20a63767ee928cb77b51f7d545
DanielCoyle <dancoyle89@gmail.com>
2025-03-17 10:25:04 -0400

    test coverage

--- packages/chat-db/vitest.config.js
+++ packages/chat-db/vitest.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  root: "src",
   test: {
-    globals: true,
-    environment: "node",
-    // setupFiles: './test/setup.js',
-    coverage: {
-      reporter: ["text", "json", "html"],
-      exclude: ["node_modules/", "test/"],
-    },
+    exclude: ["dist"],
   },
 });
