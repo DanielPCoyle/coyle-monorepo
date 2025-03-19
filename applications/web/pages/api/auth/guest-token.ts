bdca8633f97da67c6af86de4b09a1fba546cfa74
DanielCoyle <dancoyle89@gmail.com>
2025-03-19 15:21:21 -0400

    ok

--- applications/web/pages/api/auth/guest-token.ts
+++ applications/web/pages/api/auth/guest-token.ts
@@ -7,6 +7,18 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
+
+  res.setHeader("Access-Control-Allow-Origin", "https://shop.philaprints.com");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With, Content-Type, Authorization"
+  );
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end(); // Preflight request response
+  }
+
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method Not Allowed" });
   }
