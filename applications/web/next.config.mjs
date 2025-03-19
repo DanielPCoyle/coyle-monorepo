26726c67801358ef543663a8ca2aed1b7451a188
DanielCoyle <dancoyle89@gmail.com>
2025-03-19 14:46:27 -0400

    cors?

--- applications/web/next.config.mjs
+++ applications/web/next.config.mjs
@@ -42,22 +42,6 @@ const nextConfig = {
       },
     ],
   },
-  async headers() {
-    return [
-      {
-        // matching all routes
-        source: "/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "https://shop.philaprints.com" },
-          { key: "Access-Control-Allow-Origin", value: "https://www.philaprints.com" },
-          { key: "Access-Control-Allow-Origin", value: "https://philaprints.com" },
-          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      },
-    ]
-  },
   async rewrites() {
     return [
       {
