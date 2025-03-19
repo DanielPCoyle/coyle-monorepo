2d4df6095f410e2fb19e058d9360b1b694b8fce2
DanielCoyle <dancoyle89@gmail.com>
2025-03-19 14:41:31 -0400

    cors

--- applications/web/next.config.mjs
+++ applications/web/next.config.mjs
@@ -44,18 +44,18 @@ const nextConfig = {
   },
   async headers() {
     return [
-        {
-            // matching all API routes
-            source: "/api/:path*",
-            headers: [
-              { key: "Access-Control-Allow-Credentials", value: "true" },
-              { key: "Access-Control-Allow-Origin", value: "https://shop.philaprints.com" },
-              { key: "Access-Control-Allow-Origin", value: "https://www.philaprints.com" },
-              { key: "Access-Control-Allow-Origin", value: "https://philaprints.com" },
-                { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-            ]
-        }
+      {
+        // matching all routes
+        source: "/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "https://shop.philaprints.com" },
+          { key: "Access-Control-Allow-Origin", value: "https://www.philaprints.com" },
+          { key: "Access-Control-Allow-Origin", value: "https://philaprints.com" },
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      },
     ]
   },
   async rewrites() {
