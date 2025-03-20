c03cca1b7af6e7d0295e743d7f5979bfd20f0530
DanielCoyle <dancoyle89@gmail.com>
2025-03-20 02:07:54 -0400

    color corrections

--- packages/chat-ui/src/assets/svg/ImageSvg.tsx
+++ packages/chat-ui/src/assets/svg/ImageSvg.tsx
@@ -6,7 +6,7 @@ export const ImageSvg = () => {
       xmlns="http://www.w3.org/2000/svg"
       width="36"
       height="36"
-      fill="white"
+      fill="currentColor"
       className="bi bi-images"
       viewBox="0 0 16 16"
     >
